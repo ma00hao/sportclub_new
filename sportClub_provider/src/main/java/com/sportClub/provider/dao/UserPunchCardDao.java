5655a2e6f75141d37526241613d864c6c4cd3348
muziLee226 <1797063935@qq.com>
2020-08-24 19:27:13 +0800

    图片上传  8/24

--- sportClub_provider/src/main/java/com/sportClub/provider/dao/UserPunchCardDao.java
+++ sportClub_provider/src/main/java/com/sportClub/provider/dao/UserPunchCardDao.java
@@ -1,11 +1,18 @@
 package com.sportClub.provider.dao;
 
+import com.sportClub.pojo.UserPunchcard;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
 /**
  * @PROJECT_NAME: sportclub_new
  * @DESCRIPTION:
  * @USER: 木子Lee
  * @DATE: 2020/8/24 19:00
  */
-
-public class UserPunchCardDao {
+@Repository
+public interface UserPunchCardDao {
+    List<UserPunchcard> findAll();
+    int insert(UserPunchcard punchcard);
 }
